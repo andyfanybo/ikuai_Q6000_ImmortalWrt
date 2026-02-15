19229049149d787155c18fd8af65d603bae2bd4f
andyfanybo <andyfanybo@gmail.com>
2026-02-15 08:50:21 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Set default theme to luci-theme-argon
-#uci set luci.main.mediaurlbase='/luci-static/argon'
-#uci commit luci
+echo "Applying custom defaults..."
 
-# Disable IPV6 ula prefix
-# sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+# 自动查找 wan zone
+WAN_IDX=$(uci show firewall | grep "@zone" | grep "name='wan'" | cut -d[ -f2 | cut -d] -f1)
 
-# Check file system during boot
-# uci set fstab.@global[0].check_fs=1
-# uci commit fstab
+if [ -n "$WAN_IDX" ]; then
+    uci set firewall.@zone[$WAN_IDX].input='ACCEPT'
+    uci set firewall.@zone[$WAN_IDX].forward='ACCEPT'
+    uci commit firewall
+fi
 
-#exit 0
+# 关闭 DNS 重绑定保护
+uci -q set dhcp.@dnsmasq[0].rebind_protection='0'
+uci -q commit dhcp
+
+echo "Custom defaults applied."
+exit 0
